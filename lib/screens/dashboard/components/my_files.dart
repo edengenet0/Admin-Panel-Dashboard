d4030a1531ca3d14f65851a0aa5d394d665a245d
edengenet0 <edengenet157@gmail.com>
2023-12-07 11:05:42 +0300

    minor edit

--- lib/screens/dashboard/components/my_files.dart
+++ lib/screens/dashboard/components/my_files.dart
@@ -19,7 +19,7 @@ class MyFiles extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Text(
-              "My Files",
+              "Files",
               style: Theme.of(context).textTheme.titleMedium,
             ),
             ElevatedButton.icon(
